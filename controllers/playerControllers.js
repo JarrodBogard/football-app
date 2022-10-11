63913301637ee1975f5d46029639aa730523964a
Jarrod Bogard <jarrodbogard@gmail.com>
2022-10-11 16:40:05 -0500

    changed create query back

--- controllers/playerControllers.js
+++ controllers/playerControllers.js
@@ -50,12 +50,12 @@ const create = (req, res) => {
     first_name,
     last_name,
   ]);
-  pool.query(sql, (err, players) => {
+  pool.query(sql, (err, row) => {
     if (err) {
       console.log({ message: "Error occurred: " + err });
       return res.status(500).send("An unexpected error occurred");
     }
-    res.json(players.row);
+    res.json(row);
   });
 };
 
