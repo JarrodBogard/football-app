0c061629692fb656e2a6ec2c9a44f78151fd9fda
Jarrod Bogard <jarrodbogard@gmail.com>
2022-10-13 11:39:04 -0500

    removed try catch

--- middleware/requireAuth.js
+++ middleware/requireAuth.js
@@ -8,18 +8,13 @@ const requireAuth = (req, res, next) => {
 
   const token = authorization.split(" ")[1];
 
-  try {
-    const { id } = jwt.verify(token, process.env.SECRET);
-    console.log(id);
+  const { id } = jwt.verify(token, process.env.SECRET);
+  console.log(id);
 
-    req.user = { id };
-    console.log(req.user);
+  req.user = { id };
+  console.log(req.user);
 
-    next();
-  } catch (error) {
-    console.log(error);
-    res.status(401).json({ error: "Request is not authorized" });
-  }
+  next();
 };
 
 module.exports = requireAuth;
