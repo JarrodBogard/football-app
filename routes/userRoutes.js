57d26e853e2d498f860d98fb9f8e539dcb02fba5
Jarrod Bogard <jarrodbogard@gmail.com>
2022-10-12 20:38:59 -0500

    added course to user router

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,6 +10,9 @@ const {
   update,
   remove,
 } = require("../controllers/userControllers");
+const cors = require("cors");
+
+router.use(cors);
 
 router.get("/", list);
 router.get("/:id", show);
