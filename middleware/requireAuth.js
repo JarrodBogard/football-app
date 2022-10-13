d1e77902dcbeabea3f04e030d7b07d7e92cdb413
Jarrod Bogard <jarrodbogard@gmail.com>
2022-10-12 19:36:17 -0500

    added auth middleware in middleware folder and player routes

---
+++ middleware/requireAuth.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+const { createPool } = require("mysql");
+
+const requireAuth = async (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json({ error: "Authorization token required" });
+  }
+
+  const token = authorization.split(" ")[1];
+
+  try {
+    const { id } = jwt.verify(token, process.env.SECRET);
+    console.log(id, "id from middleware");
+
+    req.user = { id };
+    console.log(req.user, "req.user on middleware");
+
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({ error: "Request is not authorized" });
+  }
+};
+
+module.exports = requireAuth;
