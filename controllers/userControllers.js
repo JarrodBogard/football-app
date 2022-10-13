96ef1411b262cb551b49f91cb2bbeada2eee536c
Jarrod Bogard <jarrodbogard@gmail.com>
2022-10-12 21:49:01 -0500

    changes

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -31,8 +31,6 @@ const login = async (req, res) => {
     if (match) {
       const token = createToken(rows.id);
       res.json({ rows, token });
-      // const { id, password, username } = rows[0];
-      // res.json({ id, password, username, token });
     }
   });
 };
